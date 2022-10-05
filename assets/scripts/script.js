30449f6ad97311a638dfac6887e7ef0b8ca2ad91
Mark Gardner <themarkgardner@gmail.com>
2022-10-05 01:19:49 -0500

    Added ID elements for clickable areas. Added event listeners for clickable elements.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -5,41 +5,28 @@ var currentDate = moment().format("dddd MMM Do YYYY")
 $(".currentDate").text(currentDate);
 //populate calendar page
 for (let i = 1; i < 25; i++) {
-    let currentTime = moment().format("h a");
-    var calendarRow = $("<section></section>").addClass("row containerRow ");
-    if (i === 12) {
-        let hour = 12 + " pm";
-        var hourCol1 = $("<div></div>").addClass("col-2 border border-dark rounded-left p-2 text-right").text(hour);
-        if (currentTime === hour) {
-            calendarRow = $("<section></section>").addClass("row containerRow bg-success");
-        }
-    } else if (i === 24) {
-        let hour = 12 + " am";
-        var hourCol1 = $("<div></div>").addClass("col-2 border border-dark rounded-left p-2 text-right").text(hour);
-        if (currentTime === hour) {
-            calendarRow = $("<section></section>").addClass("row containerRow bg-success");
-        }
-    } else if (i > 12) {
-        let hour = i - 12 + " pm";
-        var hourCol1 = $("<div></div>").addClass("col-2 border border-dark rounded-left p-2 text-right").text(hour);
-        if (currentTime === hour) {
-            calendarRow = $("<section></section>").addClass("row containerRow bg-success");
-        }
+    let currentTime = moment().format("HH");
+    let iTime = moment(i, "HH").format("HH");
+   console.log(iTime);
+    let timeLabel = moment( i , 'HH').format('h a'); 
+    console.log(currentTime)
+    
+  // if current time matches, turn row red
+    if (currentTime === iTime) {
+        var calendarRow = $("<section></section>").addClass("row containerRow border border-dark rounded bg-danger");
+    } 
+    else if (currentTime < iTime) {
+        var calendarRow = $("<section></section>").addClass("row containerRow border border-dark rounded bg-success");
+    } else {
+        var calendarRow = $("<section></section>").addClass("row containerRow border border-dark rounded");
     }
-    else {
-        let hour = i + " am";
-        var hourCol1 = $("<div></div>").addClass("col-2 border border-dark rounded-left p-2 text-right").text(hour);
-        if (currentTime === hour) {
-            calendarRow = $("<section></section>").addClass("row containerRow bg-success");
-        }
-    };
-    var hourCol2 = $("<div></div>").addClass("border border-dark col-8 p-2");
-    var hourCol3 = $("<div></div>").addClass("col-2 border border-dark rounded-right p-2");
+
+    var hourCol1 = $("<div></div>").addClass("col-2  p-2 text-right schedTime").attr('id','eventTime').text(timeLabel);
+    var hourCol2 = $("<a></a>").addClass(" col-9 border-left border-right border-dark p-2 ").attr('id', 'scheduleEvent');
+    var hourCol3 = $("<a></a>").addClass("col-1  p-2 ").attr('id', 'deleteEvent');
     $(".calendar").append(calendarRow);
     calendarRow.append(hourCol1, hourCol2, hourCol3);
 };
-
-// for (let i = 1; i < 25; i++) {
-//     let currentTime = moment().format("h a");
-//     // console.log(currentTime);
-//     if (currentTime === )
+$('#eventTime').on('click', buildEvent);
+$('#scheduleEvent').on('click', buildEvent);
+$('#deleteEvent').on('click', clearEvent);
