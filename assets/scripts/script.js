20cfd9cfdddcb54d2d52a4a138b16f908c22c0b1
Mark Gardner <themarkgardner@gmail.com>
2022-10-05 10:02:04 -0500

    started code for storing calendar in local storage

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -16,24 +16,26 @@ for (let i = 0; i < 24; i++) {
     } else {
         var calendarRow = $("<section></section>").addClass("row containerRow border  border-dark border-left-0  rounded-right");
     }
-    var hourCol1 = $("<div></div>").addClass("col-2  p-3 text-right schedTime").attr('id', 'eventTime').html("<h4>" + timeLabel + "</h4>");
-    var hourCol2 = $("<input></input>").addClass(" col-9 d-flex align-top pl-2 pr-2 border-0 p-0 text-top bg-transparent").attr('id', 'scheduleEvent').attr("type", "text").attr('placeholder', scheduledEvent + i);
+    var hourCol1 = $("<div></div>").addClass("col-2  p-3 text-right schedTime").attr('id', 'eventTime' + i).html("<h4>" + timeLabel + "</h4>");
+    var hourCol2 = $("<input></input>").addClass(" col-9 d-flex align-top pl-2 pr-2 border-0 p-0 text-top bg-transparent").attr('id', 'scheduleEvent'+i).attr("type", "text");
     var hourCol3 = $("<button></button>").addClass("col-1 d-flex p-3 bg-info text-light justify-content-center border-0 align-middle").attr('id', 'saveEvent').html('<span class="material-symbols-outlined align-self-center">save</span>');
     $(".calendar").append(calendarRow);
     calendarRow.append(hourCol1, hourCol2, hourCol3);
 };
 
 
-$('#eventTime').on('click', buildEvent);
-$('#scheduleEvent').on('click', buildEvent);
 $('#saveEvent').on('click', storeEvent);
 
 
-function buildEvent () {
 
-};
 
 
 function storeEvent () {
-
+    for (let i = 0; i < 24; i++) {
+        let hour = $('#eventTime' + i);
+        let eventDescription = $('#scheduleEvent' + i).text();
+        scheduledEvent[i] = [hour, eventDescription];
+    };
+    console.log(scheduledEvent)
+    localStorage.setItem("calendarEvents", JSON.stringify(scheduledEvent));
 };
