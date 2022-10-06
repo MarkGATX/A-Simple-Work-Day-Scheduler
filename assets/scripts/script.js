6c49026df30de8a8881ef448564ff933fc38233d
Mark Gardner <themarkgardner@gmail.com>
2022-10-05 22:13:41 -0500

    Added local storage of events with click. Added bootstrap classes to help vertically align text between elements. Delegated listeners and returned .closest to move across the DOM for data.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,41 +1,63 @@
 var currentDate = moment().format("dddd MMM Do YYYY")
-var scheduledEvent = [];
+
 
 //Fill in current date
 $(".currentDate").text(currentDate);
 //populate calendar page
 for (let i = 0; i < 24; i++) {
+    //use moment.js to log times in first column and compare to current time
     let currentTime = moment().format("HH MMMM Do");
     let iTime = moment(i, "HH MMMM Do").format("HH MMMM Do");
     let timeLabel = moment(i, 'HH').format('h a');
-    // if current time matches, turn row red
+    // change color based on whether time is before, at, or after current hour
     if (currentTime === iTime) {
-        var calendarRow = $("<section></section>").addClass("row containerRow border border-dark border-left-0  rounded-right bg-danger");
+        var calendarRow = $("<section></section>").addClass("row containerRow border border-dark border-left-0 rounded-right bg-danger");
     } else if (currentTime < iTime) {
-        var calendarRow = $("<section></section>").addClass("row containerRow border border-dark border-left-0  rounded-right bg-success");
+        var calendarRow = $("<section></section>").addClass("row containerRow border border-dark border-left-0 rounded-right bg-success");
     } else {
-        var calendarRow = $("<section></section>").addClass("row containerRow border  border-dark border-left-0  rounded-right");
+        var calendarRow = $("<section></section>").addClass("row containerRow border  border-dark border-left-0 rounded-right");
     }
-    var hourCol1 = $("<div></div>").addClass("col-2  p-3 text-right schedTime").attr('id', 'eventTime' + i).html("<h4>" + timeLabel + "</h4>");
-    var hourCol2 = $("<input></input>").addClass(" col-9 d-flex align-top pl-2 pr-2 border-0 p-0 text-top bg-transparent").attr('id', 'scheduleEvent'+i).attr("type", "text");
+    var hourCol1 = $("<div></div>").addClass("col-2  p-3 text-right bg-white schedTime pt-4 align-baseline").attr('id', 'eventTime' + i).html("<h4>" + timeLabel + "</h4>");
+    var hourCol2 = $("<input></input>").addClass(" col-9 d-flex align-top pl-2 pr-2  border-0 p-0 text-top bg-transparent align-baseline").attr('id', 'scheduleEvent' + i).attr("type", "text");
     var hourCol3 = $("<button></button>").addClass("col-1 d-flex p-3 bg-info text-light justify-content-center border-0 align-middle").attr('id', 'saveEvent').html('<span class="material-symbols-outlined align-self-center">save</span>');
+    //populate schedule with saved events
+    var scheduledEvent = JSON.parse(localStorage.getItem("savedEvents"));
+    for (let i = 0; i<scheduledEvent.length; i++) {
+        console.log(scheduledEvent[i][0]);
+        console.log(timeLabel);
+        console.log(scheduledEvent[i][1]);
+        if (scheduledEvent[i][0] === timeLabel) {
+            hourCol2.val(scheduledEvent[i][1]);
+        }
+    };
     $(".calendar").append(calendarRow);
     calendarRow.append(hourCol1, hourCol2, hourCol3);
+
 };
 
 
-$('#saveEvent').on('click', storeEvent);
+$('#calendarContainer').on('click', '#saveEvent', storeEvent);
 
 
-
-
-
-function storeEvent () {
-    for (let i = 0; i < 24; i++) {
-        let hour = $('#eventTime' + i);
-        let eventDescription = $('#scheduleEvent' + i).text();
-        scheduledEvent[i] = [hour, eventDescription];
-    };
-    console.log(scheduledEvent)
-    localStorage.setItem("calendarEvents", JSON.stringify(scheduledEvent));
+function storeEvent(e) {
+    // check for any saved values
+    var scheduledEvent = JSON.parse(localStorage.getItem("savedEvents"));
+    console.log(scheduledEvent);
+    //initialize scores variable if doesn't already exist in local storage
+    if (scheduledEvent === null) {
+        scheduledEvent = [];
+        localStorage.setItem("savedEvents", JSON.stringify(scheduledEvent));
+    }
+    // get text input values and values of time
+    var clicked = e.target.closest('#saveEvent');
+    var clickedEventData = clicked.previousSibling.value;
+    if (clickedEventData === "") {
+        return
+    }
+    var clickedEventTime = clicked.previousSibling.previousSibling.textContent;
+    //put time and event data in new arry
+    var newEvent = [clickedEventTime, clickedEventData];
+    //push newEvents into existing event array
+    scheduledEvent.push(newEvent);
+    localStorage.setItem("savedEvents", JSON.stringify(scheduledEvent));
 };
